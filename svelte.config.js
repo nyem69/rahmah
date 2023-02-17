c74b45e3dfeb68707bdd0699ba99c92f8c706ba6
nyem <1857991+nyem69@users.noreply.github.com>
2023-02-17 23:53:11 +0800

    adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+//import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+//		adapter: adapter()
+		adapter: adapter({ pages: 'docs', assets: 'docs', fallback: 'app.html', precompress: true }),
 	}
 };
 
