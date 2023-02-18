6479c71babb4ecdbf1a9f69faff408ea89dac3e4
nyem <1857991+nyem69@users.noreply.github.com>
2023-02-19 01:57:46 +0800

    svg logo

---
+++ src/routes/+layout.server.js
@@ -0,0 +1,39 @@
+import * as d3 from "d3";
+import d3comparator from '$lib/d3comparator.js';
+import {DB_URL} from '$env/static/private';
+
+let dbg=0;
+let data = {};
+
+
+dbg&&console.log('+layout.server.js START');
+dbg&&console.log('DB_URL',DB_URL);
+
+
+export async function load() {
+
+	const url = DB_URL+"/list/1/200";
+	dbg&&console.log('url',url);
+
+	await d3.json(url)
+	.then(res=>{
+
+		dbg&&console.log('res', res);
+		data.list = res;
+
+	});
+
+
+
+
+	//=====================
+	//
+	//=====================
+	dbg&&console.log('data', data);
+	dbg&&console.log('+layout.server.js END');
+
+  return data;
+
+}
+
+
